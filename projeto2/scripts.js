92322b3dc07d9f3ed77d590cd23cb8c4f74e77a4
Lorraine Cristina <89432049+lorraine-crst@users.noreply.github.com>
2022-03-15 11:49:28 -0300

    Update scripts.js

--- projeto2/scripts.js
+++ projeto2/scripts.js
@@ -5,3 +5,5 @@ nome = window.prompt('Entre com seu nome: ');
 idade = window.prompt('Digite a sua idade: ');
 linguagemPorgramação = window.prompt('Qual linguagem de programação você está aprendendo: ')
 document.write("Olá " + nome +  " você tem lo " + idade + " anos",  " e já está aprendendo " + linguagemPorgramação + "!");
+
+console.log('Estamos "revisando" os conceitos basicos de JS')
