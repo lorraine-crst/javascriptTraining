89a525e43e5f52ff4c65b721b9b162bfcbe7f1ac
Lorraine Cristina <89432049+lorraine-crst@users.noreply.github.com>
2022-03-16 12:54:05 -0300

    feat: projeto 3

---
+++ projeto3/scripts.js
@@ -0,0 +1,9 @@
+console.log("Hello, World!");
+
+let myNumber = 4;
+let myString = 'Variables are great';
+let myBoolean = false; 
+
+console.log("myNumber is equal to " + myNumber);
+console.log("myString is equal to " + myString);
+console.log("myBoolean is equal to " + myBoolean);
