99eed926c54486e8d450bfb096d4679a678143b5
Lorraine Cristina <89432049+lorraine-crst@users.noreply.github.com>
2022-03-17 20:47:19 -0300

    Create index.js

---
+++ projeto2/index.js
@@ -0,0 +1,23 @@
+/**
+* Siga os passos comentados para escrever um pedaço de código.
+*/
+
+// 1. Declare a variável e atribua a ela um array vazio. Certifique-se de que o nome da variável indica que ela contém mais de 1 item. Por exemplo, itens em vez de item.
+
+
+// 2. Escreva uma instrução console.log que explique em palavras o que você acha que é o valor do array.
+
+
+// 3. Escreva uma instrução console.log que registre o array.
+
+
+// 4. Crie uma nova variável com um array que tenha 3 de seus animais favoritos, cada um em uma string diferente. Certifique-se de que o nome das variáveis ​​diga algo sobre o que a variável contém.
+
+
+// 5. Escreva uma instrução console.log que registre o segundo array.
+
+
+// 6. Adicione uma instrução que adiciona outra string ("Piglet)" ao array de animais.
+
+
+// 7. Escreva uma instrução console.log que registre o segundo array!
