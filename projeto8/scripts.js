2e45e5d95ba239ef405afe9403368607759a06b9
Lorraine Cristina <89432049+lorraine-crst@users.noreply.github.com>
2022-03-17 12:36:30 -0300

    feat: projeto8

---
+++ projeto8/scripts.js
@@ -0,0 +1,10 @@
+//Defina uma função de seta divideByTwoque aceite um número e retorne esse número dividido por 2.
+
+// TODO: define the function divideByTwo here
+function divideByTwo(number){
+    return number / 2;
+}
+console.log(divideByTwo(4));
+console.log(divideByTwo(8));
+console.log(divideByTwo(12));
+console.log(divideByTwo(6));
