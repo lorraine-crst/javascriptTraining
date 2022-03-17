d25771ebc2465ed86e370c691f114ef6f888c712
Lorraine Cristina <89432049+lorraine-crst@users.noreply.github.com>
2022-03-17 12:35:52 -0300

    feat: projeto5

---
+++ projeto5/scripts.js
@@ -0,0 +1,15 @@
+/*Neste exercício, você faz o seguinte:
+
+Conecte o firstNamee lastNamepara construir a variável fullName, mas com um espaço ( " ") entre o nome e o sobrenome.
+Multiplique a variável myNumberpor 2 e coloque o resultado em meaningOfLife. */
+
+var firstName = "John";
+var lastName = "Smith";
+var myNumber = 21;
+
+// TODO: change the following code
+var fullName = firstName + " " + lastName;
+var meaningOfLife = myNumber * 2;
+
+console.log("The name is " + fullName);
+console.log("The meaning of life is " + meaningOfLife);
