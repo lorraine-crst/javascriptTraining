a811e33a2f07f74f2aece04dc631ac36cb05b0e5
Lorraine Cristina <89432049+lorraine-crst@users.noreply.github.com>
2022-03-17 12:35:18 -0300

    update: projeto3

--- projeto3/scripts.js
+++ projeto3/scripts.js
@@ -1,5 +1,13 @@
+//Você deve imprimir no console a frase "Hello, World!".
 console.log("Hello, World!");
 
+
+/*Você deve definir as seguintes variáveis:
+
+Um número chamado myNumber que contém o número 4;
+Uma string chamada myString que contém a sentença ;Variables are great.
+Um booleano chamado myBoolean que contém o valor false;
+ */
 let myNumber = 4;
 let myString = 'Variables are great';
 let myBoolean = false; 
