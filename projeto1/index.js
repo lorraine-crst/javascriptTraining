32271a96c6b05f75badbfcbb7ff32004994cbbec
Lorraine Cristina <89432049+lorraine-crst@users.noreply.github.com>
2022-03-17 20:47:11 -0300

    Create index.js

---
+++ projeto1/index.js
@@ -0,0 +1,86 @@
+/* 
+* Escreva uma instrução, usando a função console.log(). Deve cumprir os seguintes requisitos:
+* Recebe uma string como argumento
+* A string deve conter a mensagem "Hello world!"
+* Execute o arquivo 10 vezes, cada vez usando a frase em diferentes idiomas
+*/
+
+console.log('Olá Mundo!!'); //portugues
+console.log('Hello World!!'); //ingles
+console.log('Halo, Dunia!'); //indonesio
+console.log('Ciao, Mondo!'); //italiano 
+console.log('Hei Verden!!'); // noruegues
+console.log('Bonjour le Monde!!'); //frances
+console.log('Hallo Welt!!'); //alemao
+console.log('Aloha Honua!!'); //havaiano
+console.log('Moien Welt!!'); //luxemburgues
+console.log('Hello Mundo!!'); //filipino
+
+/**
+* Considere este código, ele contém um erro de sintaxe.
+* Corrija para que ao executar este arquivo ele mostre a mensagem 'I'm awesome!'
+
+console.log('I'm awesome'!;
+*/
+
+console.log("I'm awesome!");
+
+/**
+* Siga os passos comentados para escrever um pedaço de código.
+* Em seguida, execute-o para ver se você está correto!
+*/
+
+// 1. Declare sua variável numberX. Não inicialize (o que significa, não dê um valor inicial) ainda
+
+
+// 2. Adicione uma instrução console.log que explique em palavras o que você acha que é o valor de x
+
+
+// 3. Adicione uma instrução console.log que registre o valor de numberX.
+
+
+// 4. Agora inicialize sua variável numberX com um número (também chamado de inteiro em termos de ciência da computação)
+
+
+// 5. Em seguida, adicione uma instrução console.log que explica o que você acha que é o valor de numberX
+
+
+// 6. Adicione uma instrução console.log que registra o valor de numberX
+
+let numberX; //1
+console.log('Eu penso que o numero de X por enquanto está indefinido'); //2
+console.log(numberX); //3
+
+numberX = 10; // 4
+console.log('O valor de numberX foi definido para 10'); //5
+console.log(numberX);
+
+/**
+* Siga os passos comentados para escrever um pedaço de código.
+* Em seguida, execute-o para ver se você está correto!
+*/
+
+// 1. Declare uma variável myString e atribua uma string a ela. Use seu nome completo, incluindo espaços, como o conteúdo da string.
+
+
+// 2. Escreva uma instrução console.log na qual você explique em palavras o que você acha que é o valor da string.
+
+
+// 3. Agora console.log a variável myString.
+
+
+// 4. Agora reatribua à variável myString uma nova string.
+
+
+// 5. Assim como você fez antes, escreva uma instrução console.log que explica em palavras o que você acha que será registrado no console.
+
+
+// 6. Agora console.log myString novamente.
+
+let myString = 'Lorraine cristina'; //1
+console.log('A variavel acima esta recebendo valor do meu nome e sobrenome'); //2
+console.log(myString); //3
+
+myString = 'Meu nome é Lorraine cristina';//4
+console.log('Estou retribuindo um novo valor para a minha string crriada anteriormente');
+console.log(myString);
