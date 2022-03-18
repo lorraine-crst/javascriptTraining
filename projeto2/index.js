aea1c2d698bdac7c1784f39b6f5af0cb4b0c6f76
Lorraine Cristina <89432049+lorraine-crst@users.noreply.github.com>
2022-03-18 12:55:37 -0300

    Update index.js

--- projeto2/index.js
+++ projeto2/index.js
@@ -21,3 +21,25 @@
 
 
 // 7. Escreva uma instrução console.log que registre o segundo array!
+
+let numeros = []; //1
+console.log('o array acima esta vazio de informaçoes'); //2
+console.log(numeros);//3
+
+var animais = ['gato', 'hamster', 'coelho']; //4
+console.log(animais); //5
+animais.push('Piglet'); //6
+console.log(animais); //7
+
+
+/**
+* Siga os passos comentados para escrever um pedaço de código.
+*/
+
+// 1. Declare uma variável chamada mySentence e inicialize-a com a seguinte string: "Programming is so interest!".
+
+
+// 2. Descubra (usando o Google) como obter o comprimento de mySentence. Em seguida, escreva uma instrução console.log para registrar o comprimento de mySentence.
+
+var mySentence = 'Programming is so interest!'; //1
+console.log(mySentence.length); //2
