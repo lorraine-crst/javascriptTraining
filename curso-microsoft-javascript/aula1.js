3de9ee980ad0af480d32a2a514313e9c3f27e319
Lorraine Cristina <89432049+lorraine-crst@users.noreply.github.com>
2022-03-21 21:52:55 -0300

    Update aula1.js

---
+++ curso-microsoft-javascript/aula1.js
@@ -0,0 +1,6 @@
+/*Exercício
+Você já brincou de tentar adivinhar o número que seu amigo ou amiga estava pensando? Pois hoje você irá jogar esse joga contra o próprio computador!
+
+Aqui está tudo o que você precisa fazer para o desafio de hoje:
+
+Você deve criar um programinha que já inicialize com um valor específico entre 0-10 para o número que você irá adivinhar Em seguida, ele perguntará para você qual o valor que você deseja chutar e, caso você acerte, ele irá te parabenizar ou, caso erre, ele irá te dar mais 2 tentativas No fim, caso você não acerte nenhuma vez, ele irá imprimir qual era o número inicial */
