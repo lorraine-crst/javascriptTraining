d61f6567437f96e2683188879286c9b5274ae70c
Lorraine Cristina <89432049+lorraine-crst@users.noreply.github.com>
2022-03-27 10:28:47 -0300

    Update scripts.js

---
+++ projeto9/scripts.js
@@ -0,0 +1,14 @@
+/**
+* Queremos remover as vírgulas na string fornecida (myString), substituí-las por um espaço e registrá-las no console.
+*
+* O resultado final deve ser:
+* Olá, esta é uma frase difícil de ler
+*/
+
+let  myString  =  'olá, esta é uma frase difícil de ler';
+
+
+
+/* --- Código que testará sua solução, NÃO altere. Escreva acima desta linha --- */
+
+console.log ( myString  ===  'olá, esta é uma frase difícil de ler' ,  'Há algo errado com sua solução' ) ;
