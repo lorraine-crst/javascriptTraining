0d9ded0c94eab3d69b0a5d4dd72acc6f1f3baf78
Lorraine Cristina <89432049+lorraine-crst@users.noreply.github.com>
2022-03-28 12:24:29 -0300

    Update aula1.js

---
+++ curso-microsoft-javascript/aula1.js
@@ -0,0 +1,6 @@
+let idade = 19; 
+let mensagemExibida;
+
+mensagemExibida = idade >= 18 ? 'Você está apto para tirar carteira de motorista' : 'Você não está apto para tirar carteira de motorista, pois voce tem: ' + idade + ' anos de idade';
+
+console.log(mensagemExibida);
