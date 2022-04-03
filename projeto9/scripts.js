ace4dcf413150a90a6175f8dc36e993381c6d930
Lorraine Cristina <89432049+lorraine-crst@users.noreply.github.com>
2022-04-03 12:14:55 -0300

    Update scripts.js

--- projeto9/scripts.js
+++ projeto9/scripts.js
@@ -12,3 +12,11 @@ let  myString  =  'olá, esta é uma frase difícil de ler';
 /* --- Código que testará sua solução, NÃO altere. Escreva acima desta linha --- */
 
 console.log ( myString  ===  'olá, esta é uma frase difícil de ler' ,  'Há algo errado com sua solução' ) ;
+
+
+let idade = 19; 
+let mensagemExibida;
+
+mensagemExibida = idade >= 18 ? 'Você está apto para tirar carteira de motorista' : 'Você não está apto para tirar carteira de motorista, pois voce tem: ' + idade + ' anos de idade';
+
+console.log(mensagemExibida);
