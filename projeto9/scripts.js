55165701c501fbd40a35676d1f7786a35e38f3c7
Lorraine Cristina <89432049+lorraine-crst@users.noreply.github.com>
2022-04-09 20:51:31 -0300

    Update scripts.js

--- projeto9/scripts.js
+++ projeto9/scripts.js
@@ -20,3 +20,11 @@ let mensagemExibida;
 mensagemExibida = idade >= 18 ? 'Você está apto para tirar carteira de motorista' : 'Você não está apto para tirar carteira de motorista, pois voce tem: ' + idade + ' anos de idade';
 
 console.log(mensagemExibida);
+
+
+
+//armazenando dados 
+let newYearsResolutions = ['Keep a journal', 'Take a falconry class', 'Learn to juggle'];
+
+console.log(newYearsResolutions);
+
